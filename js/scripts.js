5d5f3ecf1d1f40ace26a305b1f19130708b26e30
Cici Harrison, Leah Leonard, Virlie Paglinawan and Zoe Zhou <cici@>
2018-07-18 19:53:42 -0700

    it will add urgency parameters to each day of the week conditional logic

--- js/scripts.js
+++ js/scripts.js
@@ -30,30 +30,85 @@ $(document).ready(function() {
 
     var newEvent = new task(name, day, startTime, endTime, urgency);
 
-    if (day === "Sunday") {
+    if (day === "Sunday" && urgency === "high") {
       $("#sun").text(newEvent.print());
-    } else if (day === "Monday") {
+      $("#sun").css("background-color", "red");
+    } else if (day === "Sunday" && urgency === "medium") {
+      $("#sun").text(newEvent.print());
+      $("#sun").css("background-color", "blue");
+    } else if (day === "Sunday" && urgency === "low") {
+      $("#sun").text(newEvent.print());
+      $("#sun").css("background-color", "orange");
+
+    } else if (day === "Monday" && urgency === "high") {
       $("#mon").text(newEvent.print());
-    } else if (day === "Tuesday") {
+      $("#mon").css("background-color", "red");
+    } else if (day === "Monday" && urgency === "medium") {
+      $("#mon").text(newEvent.print());
+      $("#mon").css("background-color", "blue");
+    } else if (day === "Monday" && urgency === "low") {
+      $("#mon").text(newEvent.print());
+      $("#mon").css("background-color", "orange");
+
+    } else if (day === "Tuesday" && urgency === "high") {
       $("#tues").text(newEvent.print());
-    } else if (day === "Wednesday") {
+      $("#tues").css("background-color", "red");
+    } else if (day === "Tuesday" && urgency === "medium") {
+      $("#tues").text(newEvent.print());
+      $("#tues").css("background-color", "blue");
+    } else if (day === "Tuesday" && urgency === "low") {
+      $("#tues").text(newEvent.print());
+      $("#tues").css("background-color", "orange");
+
+    } else if (day === "Wednesday" && urgency === "high") {
       $("#wed").text(newEvent.print());
-    } else if (day === "Thursday") {
+      $("#wed").css("background-color", "red");
+    } else if (day === "Wednesday" && urgency === "medium") {
+      $("#wed").text(newEvent.print());
+      $("#wed").css("background-color", "blue");
+    } else if (day === "Wednesday" && urgency === "low") {
+      $("#wed").text(newEvent.print());
+      $("#wed").css("background-color", "orange");
+
+    } else if (day === "Thursday" && urgency === "high") {
       $("#thurs").text(newEvent.print());
-    } else if (day === "Friday") {
+      $("#thurs").css("background-color", "red");
+    } else if (day === "Thursday" && urgency === "medium") {
+      $("#thurs").text(newEvent.print());
+      $("#thurs").css("background-color", "blue");
+    } else if (day === "Thursday" && urgency === "low") {
+      $("#thurs").text(newEvent.print());
+      $("#thurs").css("background-color", "orange");
+
+    } else if (day === "Friday" && urgency === "high") {
       $("#fri").text(newEvent.print());
-    } else if (day === "Saturday") {
+      $("#fri").css("background-color", "red");
+    } else if (day === "Friday" && urgency === "medium") {
+      $("#fri").text(newEvent.print());
+      $("#fri").css("background-color", "blue");
+    } else if (day === "Friday" && urgency === "low") {
+      $("#fri").text(newEvent.print());
+      $("#fri").css("background-color", "orange");
+
+    } else if (day === "Saturday" && urgency === "high") {
       $("#sat").text(newEvent.print());
+      $("#sat").css("background-color", "red");
+    } else if (day === "Saturday" && urgency === "medium") {
+      $("#sat").text(newEvent.print());
+      $("#sat").css("background-color", "blue");
+    } else if (day === "Saturday" && urgency === "low") {
+      $("#sat").text(newEvent.print());
+      $("#sat").css("background-color", "orange");
     }
-
+/*
     if (urgency === "high") {
-      $(".days li").css("background-color", "red");
+      $("#sun").css("background-color", "red");
     } else if (urgency === "medium") {
-      $(".days li").css("background-color", "blue");
+      $("#mon").css("background-color", "blue");
     } else if (urgency === "low") {
-      $(".days li").css("background-color", "orange");
+      $("#tues").css("background-color", "orange");
     }
-
+*/
 
     console.log(name);
     console.log(day);
